3a0fc1721b0123a95eb6a0bd5715a1ba71f4b169
ed.chin.git <edgardochin@gmail.com>
2020-04-02 11:50:57 -0500

    Add Ranking Logic

--- Sprint Challenge/northwind.py
+++ Sprint Challenge/northwind.py
@@ -85,6 +85,13 @@ def run_queries(cur):
 
 
 def run_new_queries(cur):
+    '''
+    SELECT a1.Name, a1.Sales, COUNT (a2.Sales) Sales_Rank
+    FROM Total_Sales a1, Total_Sales a2
+    WHERE a1.Sales < a2.Sales OR (a1.Sales=a2.Sales AND a1.Name = a2.Name)
+    GROUP BY a1.Name, a1.Sales
+    ORDER BY a1.Sales DESC, a1.Name DESC;
+    '''
     # ____ end of queries ___
     return
 
