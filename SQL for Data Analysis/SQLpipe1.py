156490dafa34ed4393bb280b2ac39831a02c7b1b
ed.chin.git <edgardochin@gmail.com>
2020-04-11 18:30:31 -0500

    Add dotenv

--- SQL for Data Analysis/SQLpipe1.py
+++ SQL for Data Analysis/SQLpipe1.py
@@ -1,24 +1,32 @@
-#  ________
+'''
+ __SQL Pipeline Conversion 1 __
+  Sqlite to  ElephantSQL(postgres) 
+   using SQLalchemy engine
+         pandas.read_sql_table() converts SQL->df
+         pandas.to_sql() converts df->SQL
+'''
+
 import pandas as pd
 import sqlite3
 import psycopg2
 from sqlalchemy import create_engine
 from sqlite3 import dbapi2 as sqlite
-import dotenv
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-
-def verify_output(pgres_engine, table_name):
+def verify_output(pgres_engine, table_name, schema_name):
     # ______  verify output-table contents ____
-    query = 'SELECT * FROM ' + table_name + ' LIMIT 10;'
+    #   ElephantSQL requires FROM "schema".table_name   format
+    query = 'SELECT * FROM ' + '"' + schema_name + '"' +  '.' + table_name + ' LIMIT 10;'
     for row in pgres_engine.execute(query).fetchall():
         print(row)
     return
 
 
-def run_conversion(pgres_engine, lite_engine):
+def run_conversion(pgres_engine):
     # ___ process tables ____
-    # - WARNING!  schema must already exist
+    # - WARNING!  FOR AWS : schema must already exist
     schema_name = 'lambdaRPG'
     tables = ['charactercreator_character',
               'charactercreator_character_inventory',
@@ -31,8 +39,8 @@ def run_conversion(pgres_engine, lite_engine):
               'armory_weapon']
 
     # ___ connect to  sqlite3  ____
-    # lite_engine = create_engine('sqlite+pysqlite:///rpg_db.sqlite3',
-    #                             module=sqlite)
+    lite_engine = create_engine('sqlite+pysqlite:///rpg_db.sqlite3',
+                                module=sqlite)
 
     for table_name in tables:
         print('converting........ ', table_name)
@@ -40,48 +48,36 @@ def run_conversion(pgres_engine, lite_engine):
         df = pd.read_sql_table(table_name,
                                con=lite_engine)
 
-        #  BUG ALERT! drop the dataframe index column
-        #             before executing .to_sql()
+        #  To avoid an extra SQL table column,
+        #  set dataframe index to 1st column
+        #  before executing .to_sql()
+        df.set_index(df.columns[0], inplace=True)
 
-        # ___ Convert to postgres DB____
+        # ___ Convert df to postgresDB____
         df.to_sql(table_name,
                   if_exists='replace',
                   con=pgres_engine,
                   schema=schema_name,
                   method='multi')
-        verify_output(pgres_engine, table_name)
 
+        verify_output(pgres_engine, table_name, schema_name)
     return
 
 
 def main():
-    # __ Connect to postgres w/SQLalchemy.engine ____
-    dbname = ''
-    user = ''
-    host = ''
-    password = ''
-    file = open('aws.pwd', 'r')
-    ctr = 1
-    for line in file:
-        line = line.replace('\n', '')
-        if ctr == 1: dbname = line
-        if ctr == 2: user = line
-        if ctr == 3: host = line
-        if ctr == 4: passw = line
-        ctr = ctr + 1
-
+    # __ Connect to ElephantSQL(postgres) (SQLalchemy.create_engine) ____
+    # __ credentials stored in .env __
+    dbname = os.getenv("DS_DB_NAME")
+    user = os.getenv("DS_DB_USER")
+    host = os.getenv("DS_DB_HOST")
+    passw = os.getenv("DS_DB_PASSWORD")
     pgres_str = 'postgresql+psycopg2://'+user+':'+passw+'@'+host+'/'+dbname
     pgres_engine = create_engine(pgres_str)
 
-    # ___ connect to  sqlite3 w/SQLalchemy.engine  ____
-    lite_engine = create_engine('sqlite+pysqlite:///rpg_db.sqlite3',
-                                module=sqlite)
-
-    # ____ Port sqlite to postgres ___
-    run_conversion(pgres_engine, lite_engine)
+    # ____ Port sqlite tables to postgres ___
+    run_conversion(pgres_engine)
 
     # ___ end main ___________
-
     print('Conversion successful.....')
     return
 
