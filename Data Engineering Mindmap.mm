75666b7cfefa2af9575bf03ab828e5e461fe51db
Ed Chin <edgardochin@gmail.com>
2022-05-03 21:06:54 -0500

    Add mindMap

---
+++ Data Engineering Mindmap.mm
@@ -0,0 +1,451 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#111111" CREATED="1604421336637" ID="ID_1542286867" MODIFIED="1650427366988" VGAP="4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Data Engineering
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="28"/>
+<node CREATED="1604499499640" HGAP="91" ID="ID_1736331020" LINK="https://youtu.be/pERD1Ln_Qh8" MODIFIED="1650427328815" POSITION="right" TEXT="Data Engineering Overview" VSHIFT="176">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604502556315" FOLDED="true" HGAP="82" ID="ID_1463236521" MODIFIED="1650427262840" VSHIFT="-15">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Goals</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604502614751" ID="ID_1404193258" MODIFIED="1604504982746" TEXT="Develop Data Pipelines : ETL">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+<node CREATED="1604502659305" ID="ID_905029200" MODIFIED="1604504982741" TEXT="Manage tables and data sets for analysts and Data scientists">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+<node CREATED="1604502686029" ID="ID_1420854729" MODIFIED="1604504982741" TEXT="Design with Product in mind">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1650427072519" HGAP="63" ID="ID_1461983369" MODIFIED="1650427240503" VSHIFT="1">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4"><b>Big Book of Data Engineering</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1604502701023" FOLDED="true" HGAP="56" ID="ID_1004108421" MODIFIED="1650427223285" VSHIFT="29">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Data Engineering Skills</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604502907451" ID="ID_1616700201" MODIFIED="1604504989455" TEXT="Data Modeling">
+<font NAME="Verdana" SIZE="12"/>
+<node CREATED="1606492379815" ID="ID_190864898" MODIFIED="1606492457623" TEXT="Normalization =&gt; Relational /OLTP"/>
+<node CREATED="1606492411944" ID="ID_482706235" MODIFIED="1606492463886" TEXT="Dimensional Modeling =&gt; OLAP"/>
+</node>
+<node CREATED="1604502915696" ID="ID_1556120508" MODIFIED="1604504989455" TEXT="Automation">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+<node CREATED="1604502920373" ID="ID_66645124" MODIFIED="1604504989455" TEXT="ETL Development">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+<node CREATED="1604502927193" ID="ID_1749239680" MODIFIED="1604504989455" TEXT="Product Understanding / Domain knowledge">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1605484260846" FOLDED="true" HGAP="79" ID="ID_229003440" LINK="https://youtu.be/W2Z7fbCLSTw" MODIFIED="1650425522885" POSITION="right" TEXT="7 DataBase Paradigms" VSHIFT="2">
+<arrowlink DESTINATION="ID_229003440" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_509690852" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_229003440" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_509690852" SOURCE="ID_229003440" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node CREATED="1605484299079" HGAP="23" ID="ID_25292755" MODIFIED="1606784777718" TEXT="Key-value" VSHIFT="-7">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1606784428658" ID="ID_1987887064" MODIFIED="1606784434389" TEXT="Redis"/>
+<node CREATED="1606784436172" ID="ID_1000281753" MODIFIED="1606784444779" TEXT="MemCache"/>
+</node>
+<node CREATED="1605484299079" ID="ID_1520259942" MODIFIED="1606784767379" TEXT="Wide Column" VSHIFT="12">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1606784522837" ID="ID_862412531" MODIFIED="1606784529157" TEXT="Cassandra"/>
+<node CREATED="1606784533251" ID="ID_1584345570" MODIFIED="1606784749217" TEXT="Apache HBASE"/>
+</node>
+<node CREATED="1605484299079" HGAP="19" ID="ID_751485372" MODIFIED="1606784765888" TEXT="Document" VSHIFT="17">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1605484299079" HGAP="19" ID="ID_730523096" MODIFIED="1606784764308" TEXT="Relational" VSHIFT="15">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1605484299079" ID="ID_317207364" MODIFIED="1606784762109" TEXT="Graph" VSHIFT="13">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1605484299079" ID="ID_1183763866" MODIFIED="1606784759061" TEXT="Search Engine" VSHIFT="15">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1605484299079" ID="ID_869662862" MODIFIED="1606784757294" TEXT="Multi-model" VSHIFT="15">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1604422426232" FOLDED="true" HGAP="43" ID="ID_526464767" MODIFIED="1650427358475" POSITION="left" TEXT="ACID" VSHIFT="-75">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node CREATED="1604422513788" HGAP="178" ID="ID_1872500645" MODIFIED="1604505281688" TEXT="An acronym and mnemonic device for learning and remembering the four primary attributes ensured to any transaction by a transaction manager (AKA transaction monitor).   &#xa;Concept is described in ISO/IEC 10026-1:1992 Section 4. Each of these attributes can be measured against a benchmark. In general, however, a transaction manager/monitor is designed to realize the ACID concept.&#xa;In a distributed system, one way to achieve ACID is to use a two-phase commit (2PC), which ensures that all involved sites must commit to transaction completion or none do, and the transaction is rolled back." VSHIFT="2">
+<font BOLD="true" NAME="Verdana" SIZE="12"/>
+</node>
+<node CREATED="1604502105285" HGAP="43" ID="ID_1258026717" MODIFIED="1623042530836" TEXT="Atomicity" VSHIFT="22">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604422513788" HGAP="36" ID="ID_1164720742" MODIFIED="1604505220311" TEXT="Atomicity. In a transaction involving two or more discrete pieces of information, either all of the pieces are committed or none are. " VSHIFT="-4">
+<font NAME="Verdana" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1604422594024" HGAP="39" ID="ID_1226632080" MODIFIED="1623042529710" TEXT="Consistency" VSHIFT="29">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604422594024" HGAP="13" ID="ID_442509506" MODIFIED="1604505031177" TEXT="Consistency. A transaction either creates a new and valid state of data, or, if any failure occurs, returns all data to its state before the transaction was started.  " VSHIFT="-4">
+<font NAME="Verdana" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1604422626586" HGAP="48" ID="ID_1059271975" MODIFIED="1623042496518" TEXT="Isolation" VSHIFT="41">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604422626586" HGAP="46" ID="ID_74238834" MODIFIED="1604505230291" TEXT="Isolation. A transaction in process and not yet committed must remain isolated from any other transaction." VSHIFT="-4">
+<font NAME="Verdana" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1604422633682" HGAP="45" ID="ID_1685091926" MODIFIED="1623042510224" TEXT="Durability" VSHIFT="45">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604422633682" HGAP="40" ID="ID_563220639" MODIFIED="1604505225561" TEXT="Durability. Committed data is saved by the system such that, even in the event of a failure and system restart, the data is available in its correct state." VSHIFT="-4">
+<font NAME="Verdana" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1605277304250" HGAP="7" ID="ID_1680713010" LINK="https://youtu.be/l_dph6Qu4LA" MODIFIED="1610636453412" POSITION="left" TEXT="SQL Interview Questions" VSHIFT="-51">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+</node>
+<node CREATED="1610635422897" ID="ID_1153390960" LINK="https://www.youtube.com/playlist?list=PLMT-ZFEXpkZ23IH4toeB1WDGzi8djjGps" MODIFIED="1623602517787" POSITION="left" TEXT="Advanced SQL Lessons">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1610635498948" ID="ID_43820429" LINK="https://www.youtube.com/playlist?list=PL2SsOkMTK8FBgyauYx6bVDMla9z71D_ai" MODIFIED="1623628568980" POSITION="left" TEXT="Oracle SQL Certification">
+<arrowlink DESTINATION="ID_43820429" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1853596022" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_43820429" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1853596022" SOURCE="ID_43820429" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1623628690649" ID="ID_1362848259" LINK="https://youtu.be/zDgKn0sJsx4" MODIFIED="1623629026796" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Blazing SQL (GPU)</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1623629075680" ID="ID_1249904519" LINK="https://youtu.be/6rDaBTwSZhM" MODIFIED="1623629110019" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Blazing SQL
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1605897617605" FOLDED="true" HGAP="104" ID="ID_1934317749" LINK="https://youtu.be/ztHopE5Wnpc" MODIFIED="1650427391227" POSITION="right" VSHIFT="-137">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Relational Database
+    </p>
+    <p>
+      Design Course
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1630067851869" HGAP="58" ID="ID_1639848406" LINK="https://youtu.be/i1q_XnAsEGo" MODIFIED="1650425757802" VSHIFT="17">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Create Data Sources</font></b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1605897617605" FOLDED="true" HGAP="81" ID="ID_1799955394" LINK="https://youtu.be/ztHopE5Wnpc" MODIFIED="1650426378609" POSITION="right" VSHIFT="48">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Data Warehouses <font size="3">&#160; </font>
+    </p>
+    <p>
+      <font size="3">filtered, processed data</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#111111" CREATED="1604421830157" HGAP="75" ID="ID_1490927616" LINK="https://youtu.be/wWbxnwaHhQ8" MODIFIED="1650302592570" VSHIFT="-18">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4"><b>Data Warehouse Design</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604423626698" HGAP="42" ID="ID_1474379530" MODIFIED="1650302559427" VSHIFT="-2">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Overview</font>
+    </p>
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_1474379530" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_437769856" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1474379530" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1872681789" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1474379530" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1907971537" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1474379530" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_437769856" SOURCE="ID_1474379530" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1474379530" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1872681789" SOURCE="ID_1474379530" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1474379530" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1907971537" SOURCE="ID_1474379530" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604423626698" HGAP="60" ID="ID_1538813449" MODIFIED="1604423894456" VSHIFT="-73">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Screenshot%202020-11-03%20104201.jpg" />
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_1538813449" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1818874619" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1538813449" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1488090274" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1538813449" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_77966103" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1538813449" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1818874619" SOURCE="ID_1538813449" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1538813449" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1488090274" SOURCE="ID_1538813449" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1538813449" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_77966103" SOURCE="ID_1538813449" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1604423871706" HGAP="62" ID="ID_751994062" MODIFIED="1604423877903" TEXT="" VSHIFT="79"/>
+</node>
+</node>
+</node>
+<node CREATED="1604505374363" HGAP="75" ID="ID_164998020" LINK="Dimensional%20Data%20Modeling%20in%204%20Simple%20Steps.pdf" MODIFIED="1650302605172" VSHIFT="-44">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Dimensional Data Modeling</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1605921368151" HGAP="58" ID="ID_129195311" LINK="https://youtu.be/lWPiSZf7-uQ" MODIFIED="1610636403230" TEXT="Dimensional Modeling Overview" VSHIFT="5">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1629984452310" HGAP="72" ID="ID_839291073" LINK="https://youtu.be/J326LIUrZM8" MODIFIED="1630067191205" VSHIFT="7">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="5">Data WareHousing and BI</font></b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1629984452310" HGAP="72" ID="ID_822575622" LINK="https://youtu.be/CHYPF7jxlik" MODIFIED="1650302856962" VSHIFT="7">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Data WareHouse Concepts</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1629984452310" HGAP="72" ID="ID_1298418930" LINK="https://youtu.be/patBYUGwsHE" MODIFIED="1650302893396" VSHIFT="7">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Designing Your Data Warehouse from the Ground Up</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1650425491409" HGAP="95" ID="ID_868039268" MODIFIED="1650425950433" POSITION="right" VSHIFT="19">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5"><b>Data Lakes </b></font>
+    </p>
+    <p>
+      <font size="3">&#160;unfiltered raw data</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1650426340630" HGAP="90" ID="ID_699070545" LINK="The-Data-Lakehouse.pdf" MODIFIED="1650426948020" POSITION="right" VSHIFT="15">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5"><b>Data Lakehouse </b></font>
+    </p>
+    <p>
+      <font size="3">&#160;hybrid raw data</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1623602332968" ID="ID_1398327422" MODIFIED="1623603100650" POSITION="left" TEXT="Vertica" VSHIFT="90">
+<font BOLD="true" NAME="SansSerif" SIZE="21"/>
+<node CREATED="1623602439283" ID="ID_1450728630" LINK="https://www.vertica.com/kb/TableauTT/Content/Partner/TableauTT.htm" MODIFIED="1623602800958" TEXT="Vertica website"/>
+<node CREATED="1623602852653" ID="ID_1182039704" LINK="https://academy.vertica.com/" MODIFIED="1623602877692" TEXT="Vertica Academy"/>
+</node>
+<node CREATED="1630067851869" HGAP="5" ID="ID_1773500385" LINK="https://youtu.be/wlWLBJukhj0" MODIFIED="1650425366834" POSITION="right" VSHIFT="79">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="5">The Right Path to Data Engineer</font></b>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1630067851869" FOLDED="true" HGAP="52" ID="ID_1478845608" LINK="https://youtu.be/VbAnrhwFjP0" MODIFIED="1650302809461" VSHIFT="7">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Google Data Engineer Certification</font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1640275047110" ID="ID_560417669" LINK="https://medium.com/analytics-vidhya/how-i-passed-the-gcp-professional-data-engineer-certification-in-1st-attempt-db10ca2fadc6" MODIFIED="1640275079351" TEXT="Medium Article"/>
+</node>
+</node>
+<node CREATED="1610635590066" FOLDED="true" HGAP="10" ID="ID_18290266" MODIFIED="1650426966848" POSITION="right" TEXT="Data Engineering Interviews" VGAP="2" VSHIFT="-30">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node CREATED="1610635590066" HGAP="87" ID="ID_1054364102" LINK="https://youtu.be/V42vyEU0lMQ" MODIFIED="1629984699226" VSHIFT="68">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What is the interview process for a Data Engineering position - My experience
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1629984293722" HGAP="95" ID="ID_418674290" LINK="https://youtu.be/WbqRH2r3N40" MODIFIED="1629985149292" VSHIFT="4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;1001 Data Engineering Interview Questions
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1629984452310" HGAP="106" ID="ID_241941707" LINK="https://youtu.be/9gOw3joU4a8" MODIFIED="1629985152053" VSHIFT="13">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Data Warehouse Interview Questions And Answers
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1629984452310" HGAP="106" ID="ID_1196450282" LINK="https://youtu.be/2XppoicnUJg" MODIFIED="1630067577779" VSHIFT="13">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Acing Data Engineering Interviews
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#111111" CREATED="1604421830157" HGAP="108" ID="ID_1989802420" LINK="https://youtu.be/9gOw3joU4a8" MODIFIED="1650302762422" VSHIFT="40">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="3">Data Warehouse Interview Questions</font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+</map>
