398622e7cbedae6825271d1889f5f21bb839412a
Ed Chin <edgardochin@gmail.com>
2022-06-23 17:25:56 -0500

    delete unused

--- LinkedIn/Administering a SQL Database Infrastructure/Exercise Files/Chapter 06/06_03_RecoverCorruption.sql
+++
@@ -1,9 +0,0 @@
--- switch databases
-USE WideWorldImporters;
-GO
-
--- rebuild a nonclustered index
-ALTER INDEX FK_Sales_Orders_CustomerID ON Sales.Orders DISABLE;
-GO
-ALTER INDEX FK_Sales_Orders_CustomerID ON Sales.Orders REBUILD WITH (ONLINE = ON);
-GO
