df6d32cbc9976061b36e3573853ac9086cb3b2d0
Ed Chin <edgardochin@gmail.com>
2025-05-29 12:14:54 -0500

    Add comments

--- pipelines/flights.copy.csv.sql
+++ pipelines/flights.copy.csv.sql
@@ -1,3 +1,6 @@
+-- get the flights csv file here:  wget 46.101.230.157/sql_tutorial/2007.csv.bz2 
+-- warning: it has NA's in integer fields and must be cleaned using flights.clean.csv.py
+
 -- CSV file must be in the same directory as the pgres DB files 
 --  if Binded to windows directory -->  C:/databases/pgres/pgdat    or as defined in the container
 
