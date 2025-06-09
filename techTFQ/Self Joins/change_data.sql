20e013331b112e9a257282911a2b97d0ebd55d66
Ed Chin <edgardochin@gmail.com>
2025-06-09 18:14:18 -0500

    Refactor SQL scripts for clarity and organization

---
+++ techTFQ/Self Joins/change_data.sql
@@ -0,0 +1,7 @@
+set search_path = department;
+UPDATE tblemployee
+    SET ManagerID = 10
+    WHERE ID = 4;
+UPDATE tblemployee
+    SET ManagerID = 10
+    WHERE ID = 5;
---
+++ techTFQ/Self Joins/self_joins.sql
@@ -0,0 +1,39 @@
+/**  https://csharp-video-tutorials.blogspot.com/2012/08/self-join-in-sql-server-part-14.html 
+     https://youtu.be/qnYSN_7qwgg
+**/
+-- USE techTFQ
+-- GO
+set search_path = department;
+SELECT
+    ID,
+    Employee,
+    Manager,
+    Department
+FROM
+    ( Select E.id as ID, E.Name as Employee, M.Name as Manager, D.departmentname as Department
+     from tblemployee E
+     Left Join tblemployee M
+     On E.ManagerID = M.Id
+     Left Join tbldepartment D
+     On E.departmentid = D.Id
+     ORDER BY Manager
+    ) AS EmployeeManagerDepartment
+ORDER BY Department, Manager ASC;
+
+SELECT
+    E.ID as ID,
+    E.Name as Employee,
+    E.Gender,
+    E.Salary,
+    M.Name as Manager,
+    D.departmentname as Department
+FROM
+    tblemployee E
+LEFT JOIN
+    tblemployee M ON E.ManagerID = M.Id
+LEFT JOIN
+    tbldepartment D ON E.departmentid = D.Id
+WHERE
+    M.Name IS NULL
+ORDER BY
+    E.Salary DESC;
